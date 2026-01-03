e3bf66305e0186acca11840ae385397ef532fd04
Samiur Rahman <samiremail98@gmail.com>
2026-01-03 19:47:11 +0600

    restore function f2c; fixes #2

--- functions.py
+++ functions.py
@@ -8,4 +8,4 @@ def multiply(a, b):
     return a * b
 
 def convert_fahrenheit_to_celsius(fahrenheit):
-    return multiply(subtract(fahrenheit, 32), 5 / 9) # <-- Fixed this in step 7
+    return multiply(subtract(fahrenheit, 32), 9 / 5) # <-- Fixed this in step 7
